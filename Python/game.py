f842b2f02097876783320990ef20dc84653f3c28
TheoGits <theokrist07@gmail.com>
2024-10-23 10:03:40 +0200

    python

--- Python/game.py
+++ Python/game.py
@@ -1,142 +1,39 @@
 import pygame
-import time
-import random
-
-# Initialiser pygame
 pygame.init()
 
-# Definer farger
-white = (255, 255, 255)
-yellow = (255, 255, 102)
-black = (0, 0, 0)
-red = (213, 50, 80)
-green = (0, 255, 0)
-blue = (50, 153, 213)
+# Skjerminnstillinger
+screen = pygame.display.set_mode((400, 300))
+pygame.display.set_caption("Enkelt Spill")
 
-# Skjermdimensjoner
-width = 600
-height = 400
+# Farge og posisjon
+player = pygame.Rect(175, 125, 50, 50)
+color = (255, 0, 0)
+speed = 5
 
-# Initialiser skjermen
-dis = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Snake Game')
+# Hovedløkken
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-# Klokke for oppdatering av spillet
-clock = pygame.time.Clock()
+    # Bevegelse
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        player.x -= speed
+    if keys[pygame.K_RIGHT]:
+        player.x += speed
+    if keys[pygame.K_UP]:
+        player.y -= speed
+    if keys[pygame.K_DOWN]:
+        player.y += speed
 
-# Slangens størrelse
-snake_block = 10
-snake_speed = 15
+    # Tegn på skjermen
+    screen.fill((0, 0, 0))
+    pygame.draw.rect(screen, color, player)
+    pygame.display.flip()
+    pygame.time.Clock().tick(30)
 
-# Skrifttype
-font_style = pygame.font.SysFont("bahnschrift", 25)
-score_font = pygame.font.SysFont("comicsansms", 35)
-
-# Score
-def score(score):
-    value = score_font.render("Score: " + str(score), True, yellow)
-    dis.blit(value, [0, 0])
-
-# Slangen
-def our_snake(snake_block, snake_list):
-    for x in snake_list:
-        pygame.draw.rect(dis, green, [x[0], x[1], snake_block, snake_block])
-
-# Melding til spiller
-def message(msg, color):
-    mesg = font_style.render(msg, True, color)
-    dis.blit(mesg, [width / 6, height / 3])
-
-# Spillet
-def gameLoop():
-    game_over = False
-    game_close = False
-
-    # Startposisjon for slangen
-    x1 = width / 2
-    y1 = height / 2
-
-    # Endringer i posisjon
-    x1_change = 0
-    y1_change = 0
-
-    snake_list = []
-    length_of_snake = 1
-
-    # Generer mat for slangen
-    foodx = round(random.randrange(0, width - snake_block) / 10.0) * 10.0
-    foody = round(random.randrange(0, height - snake_block) / 10.0) * 10.0
-
-    while not game_over:
-
-        while game_close:
-            dis.fill(blue)
-            message("You lost! Press C to play again or Q to quit", red)
-            score(length_of_snake - 1)
-            pygame.display.update()
-
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_q:
-                        game_over = True
-                        game_close = False
-                    if event.key == pygame.K_c:
-                        gameLoop()
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game_over = True
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    x1_change = -snake_block
-                    y1_change = 0
-                elif event.key == pygame.K_RIGHT:
-                    x1_change = snake_block
-                    y1_change = 0
-                elif event.key == pygame.K_UP:
-                    y1_change = -snake_block
-                    x1_change = 0
-                elif event.key == pygame.K_DOWN:
-                    y1_change = snake_block
-                    x1_change = 0
-
-        # Hvis slangen går utenfor skjermen
-        if x1 >= width or x1 < 0 or y1 >= height or y1 < 0:
-            game_close = True
-        x1 += x1_change
-        y1 += y1_change
-        dis.fill(black)
-
-        # Tegner maten
-        pygame.draw.rect(dis, blue, [foodx, foody, snake_block, snake_block])
-
-        snake_head = []
-        snake_head.append(x1)
-        snake_head.append(y1)
-        snake_list.append(snake_head)
-        if len(snake_list) > length_of_snake:
-            del snake_list[0]
-
-        # Hvis slangen kolliderer med seg selv
-        for x in snake_list[:-1]:
-            if x == snake_head:
-                game_close = True
-
-        our_snake(snake_block, snake_list)
-        score(length_of_snake - 1)
-
-        pygame.display.update()
-
-        # Hvis slangen spiser maten
-        if x1 == foodx and y1 == foody:
-            foodx = round(random.randrange(0, width - snake_block) / 10.0) * 10.0
-            foody = round(random.randrange(0, height - snake_block) / 10.0) * 10.0
-            length_of_snake += 1
-
-        clock.tick(snake_speed)
-
-    pygame.quit()
-    quit()
+pygame.quit()
 
 
-gameLoop()
--- Python/input.py
+++ Python/input.py
@@ -1,20 +1,23 @@
-import tkinter as tk
-
-def hent_input():
-    brukers_input = input_felt.get()
-    print(f"Brukeren skrev: {brukers_input}")
-
-# Opprett hovedvinduet
-root = tk.Tk()
-root.title("Input felt eksempel")
-
-# Opprett et input felt
-input_felt = tk.Entry(root, width=30)
-input_felt.pack(pady=10)
-
-# Opprett en knapp som henter input
-knapp = tk.Button(root, text="Hent Input", command=hent_input)
-knapp.pack(pady=10)
-
-# Kjør applikasjonen
-root.mainloop()
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+pygame.display.set_caption("Enkelt Spill")
+player = pygame.Rect(175, 125, 50, 50)
+color = (255, 0, 0)
+speed = 5
+clock = pygame.time.Clock()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]: player.x -= speed
+    if keys[pygame.K_RIGHT]: player.x += speed
+    if keys[pygame.K_UP]: player.y -= speed
+    if keys[pygame.K_DOWN]: player.y += speed
+    screen.fill((0, 0, 0))
+    pygame.draw.rect(screen, color, player)
+    pygame.display.flip()
+    clock.tick(30)
+pygame.quit()
