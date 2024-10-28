3c2bb7d61dbe1f95b791416c81b5378d54e63090
TheoGits <theokrist07@gmail.com>
2024-10-28 10:07:45 +0100

    klassekart

--- React/oppgave11_/props/src/Klassekart.jsx
+++ React/oppgave11_/props/src/Klassekart.jsx
@@ -1,31 +1,44 @@
-import { useState } from "react"
-import Props from "./Props"
+import { useState } from "react";
+import Props from "./Props";
+
+const [nyElev,setNyElev] = "";
+
 export default function Klassekart() {
-        const [klass,setKlass] = useState([
-            { id: 1, name: "Oliver"},
-            { id: 2, name: "Justine"},
-            { id: 3, name: "karoline"},
-            { id: 4, name: "Alex"},
-            { id: 5, name: "Bastian"},
-            { id: 6, name: "Kasper"},
-            { id: 7, name: "Johannes"},
-            { id: 8, name: "Mathias"},
-            { id: 9, name: "Mads"},
-            { id: 10, name: "Sander"},
-            { id: 11, name: "Sven"},
-            { id: 12, name: "Romio"},
-            { id: 13, name: "Thomas"},
-    
-        ])
-    return (
-        <div>
-            <ul>
-                {klass.map((klass) => (
-                    <li key={klass.id} >
-                        <Props name={klass.name} />
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+  const [klass, setKlass] = useState([
+    { id: 1, name: "Oliver" },
+    { id: 2, name: "Justine" },
+    { id: 3, name: "Karoline" },
+    { id: 4, name: "Jens" },
+    { id: 5, name: "Bastian" },
+    { id: 6, name: "Kasper" },
+    { id: 7, name: "Johannes" },
+    { id: 8, name: "Mathias" },
+    { id: 9, name: "Mads" },
+    { id: 10, name: "Sander" },
+    { id: 11, name: "Sven" },
+    { id: 12, name: "Romio" },
+    { id: 13, name: "Thomas" },
+  ]);
+
+  const leggTilElev = (e) => {
+    e.preventDefault();
+    setKlass([...klass, { id: klass.length + 1, name: nyElev }]);
+    setNyElev("");
+  };
+
+  return (
+    <div>
+      <ul style={{ display: "flex", flexWrap: "wrap", listStyleType: "none", padding: 0 }}>
+        {klass.map((student) => (
+          <li key={student.id} style={{ margin: "10px" }}>
+            <Props name={student.name} />
+          </li>
+        ))}
+      </ul>
+        <form onSubmit={leggTilElev}>
+            <input value={nyElev} onChange={(e) => setNyElev(e.target.value)} placeholder="Ny elev" />
+            <button>Legg til</button>
+        </form>
+    </div>
+  );
 }
